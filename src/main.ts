8774ddb81f38304edd434797ce1ed96142016061
Yayun.Xiong <Yayun.Xiong@AP-CHN-LP150039.internal.sungard.corp>
2017-01-17 15:31:09 +0800

    加上注释和错误时输出日志

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
+// 浏览器兼容
 import './polyfills.ts';
 
+// 初始化
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// 引入angular2核心包（所有组件、指令、服务等定义类时需要的装饰器，都可以通过 @angular/core 找到）
 import { enableProdMode } from '@angular/core';
+// 环境（开发、生产）
 import { environment } from './environments/environment';
+// 根模块
 import { AppModule } from './app/app.module';
 
+// 判断是否为生产环境
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+// 启动
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
