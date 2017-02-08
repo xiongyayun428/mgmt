eb2bfc9321e37a25e82ffaa403747101c68df4e9
Xiong <Yayun.Xiong@sungard.com>
2017-02-08 14:37:27 +0800

    update

--- src/main-aot.ts
+++ src/main-aot.ts
@@ -1,25 +1,22 @@
 /**
- * 基于JIT(Just in Time)编译器的动态引导
+ * 基于AOT(Ahead of Time)编译器的静态引导
  */
 
 // 浏览器兼容
 import './polyfills.ts';
 
-// 初始化
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
+
 // 引入angular2核心包（所有组件、指令、服务等定义类时需要的装饰器，都可以通过 @angular/core 找到）
 import { enableProdMode } from '@angular/core';
 // 环境（开发、生产）
 import { environment } from './environments/environment';
-// 根模块
-import { AppModule } from './app/';
-// import { AppModule } from './app/app.module';
+
+import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
 
 // 判断是否为生产环境
 if (environment.production) {
   enableProdMode();
 };
 
-// 从一个指定的NgModule中使用根组件引导应用
-platformBrowserDynamic().bootstrapModule(AppModule);
-// .catch(err => console.error(err));
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
